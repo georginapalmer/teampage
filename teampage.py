039e221c02b04e5d013c0da8432e1c9f263c434a
georginapalmer <126188860+georginapalmer@users.noreply.github.com>
2023-02-24 11:37:53 +0000

    Add files via upload

--- teampage.py
+++ teampage.py
@@ -23,41 +23,41 @@ image2 = Image.open('JorgeJeff.jpg')
 st.image(image2)
 
 #Meet the team: 
-meetus_title = '<p style="font-family:Optima; color:Black; font-size: 60px;">Meet the team:</p>'
+meetus_title = '<p style="font-family:Optima; color:Black; font-size: 45px;">Meet the team:</p>'
 st.markdown(meetus_title, unsafe_allow_html=True)
 
 
-Ewan_title = '<strong><p style="font-family:Optima; color:#17304A; font-size: 42px;">Ewan Yeo</p></strong>'
+Ewan_title = '<strong><p style="font-family:Optima; color:Black; font-size: 20px;">Ewan Yeo</p></strong>'
 st.markdown(Ewan_title, unsafe_allow_html=True)
-roleewan_title = '<p style="font-family:Optima; color:#17304A; font-size: 42px;">Role: Jorge Santos</p>'
+roleewan_title = '<p style="font-family:Optima; color:Black; font-size: 20px;">Role: Jorge Santos</p>'
 st.markdown(roleewan_title, unsafe_allow_html=True)
-image3 = Image.open('Ewan.jpg')
+image3 = Image.open('Ewan.jpeg')
 st.image(image3, caption='Ewan Yeo 2023')
 
-Rajat_title = '<strong><p style="font-family:Optima; color:#17304A; font-size: 42px;">Rajat Khole</p></strong>'
+Rajat_title = '<strong><p style="font-family:Optima; color:Black; font-size: 20px;">Rajat Khole</p></strong>'
 st.markdown(Rajat_title, unsafe_allow_html=True)
-rolerajat_title = '<p style="font-family:Optima; color:#17304A; font-size: 42px;">Role: Jeff Bridges</p>'
+rolerajat_title = '<p style="font-family:Optima; color:Black; font-size: 20px;">Role: Jeff Bridges</p>'
 st.markdown(rolerajat_title, unsafe_allow_html=True)
-image4 = Image.open('Rajat.jpg')
+image4 = Image.open('Rajat.jpeg')
 st.image(image4, caption='Rajat Khole 2023')
 
-Ethan_title = '<strong><p style="font-family:Optima; color:#17304A; font-size: 42px;">Ethan Woolley</p></strong>'
+Ethan_title = '<strong><p style="font-family:Optima; color:Black; font-size: 20px;">Ethan Woolley</p></strong>'
 st.markdown(Ethan_title, unsafe_allow_html=True)
-roleethan_title = '<p style="font-family:Optima; color:#17304A; font-size: 42px;">Role: CTO </p>'
+roleethan_title = '<p style="font-family:Optima; color:Black; font-size: 20px;">Role: CTO </p>'
 st.markdown(roleethan_title, unsafe_allow_html=True)
-image5 = Image.open('Ethan.jpg')
+image5 = Image.open('Ethan.jpeg')
 st.image(image5, caption='Ethan Woolley 2023')
 
-Charlotte_title = '<strong><p style="font-family:Optima; color:#17304A; font-size: 42px;">Charlotte Guillemot</p></strong>'
+Charlotte_title = '<strong><p style="font-family:Optima; color:Black; font-size: 20px;">Charlotte Guillemot</p></strong>'
 st.markdown(Charlotte_title, unsafe_allow_html=True)
-rolecharlotte_title = '<p style="font-family:Optima; color:#17304A; font-size: 42px;">Role: Creative director </p>'
+rolecharlotte_title = '<p style="font-family:Optima; color:Black; font-size: 20px;">Role: Creative director </p>'
 st.markdown(rolecharlotte_title, unsafe_allow_html=True)
-image6 = Image.open('Charlotte.jpg')
+image6 = Image.open('Charlotte.jpeg')
 st.image(image6, caption='Charlotte Guillemot 2023')
 
-Georgina_title = '<strong><p style="font-family:Optima; color:#17304A; font-size: 42px;">Georgina Palmer</p></strong>'
+Georgina_title = '<strong><p style="font-family:Optima; color:Black; font-size: 20px;">Georgina Palmer</p></strong>'
 st.markdown(Georgina_title, unsafe_allow_html=True)
-rolegeorgina_title = '<p style="font-family:Optima; color:#17304A; font-size: 42px;">Role: CFO </p>'
+rolegeorgina_title = '<p style="font-family:Optima; color:Black; font-size: 20px;">Role: CFO </p>'
 st.markdown(rolegeorgina_title, unsafe_allow_html=True)
-image7 = Image.open('georgina.jpg')
+image7 = Image.open('Georgina.jpeg')
 st.image(image7, caption='Georgina Palmer 2023')
