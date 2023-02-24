65673e45c7aff0b0f6fc6cf59b0b537188e76469
georginapalmer <126188860+georginapalmer@users.noreply.github.com>
2023-02-24 11:45:38 +0000

    Add files via upload

--- teampage.py
+++ teampage.py
@@ -31,33 +31,33 @@ Ewan_title = '<strong><p style="font-family:Optima; color:Black; font-size: 20px
 st.markdown(Ewan_title, unsafe_allow_html=True)
 roleewan_title = '<p style="font-family:Optima; color:Black; font-size: 20px;">Role: Jorge Santos</p>'
 st.markdown(roleewan_title, unsafe_allow_html=True)
-image3 = Image.open('Ewan.jpeg')
+image3 = Image.open('Ewan.jpg')
 st.image(image3, caption='Ewan Yeo 2023')
 
 Rajat_title = '<strong><p style="font-family:Optima; color:Black; font-size: 20px;">Rajat Khole</p></strong>'
 st.markdown(Rajat_title, unsafe_allow_html=True)
 rolerajat_title = '<p style="font-family:Optima; color:Black; font-size: 20px;">Role: Jeff Bridges</p>'
 st.markdown(rolerajat_title, unsafe_allow_html=True)
-image4 = Image.open('Rajat.jpeg')
+image4 = Image.open('Rajat.jpg')
 st.image(image4, caption='Rajat Khole 2023')
 
 Ethan_title = '<strong><p style="font-family:Optima; color:Black; font-size: 20px;">Ethan Woolley</p></strong>'
 st.markdown(Ethan_title, unsafe_allow_html=True)
 roleethan_title = '<p style="font-family:Optima; color:Black; font-size: 20px;">Role: CTO </p>'
 st.markdown(roleethan_title, unsafe_allow_html=True)
-image5 = Image.open('Ethan.jpeg')
+image5 = Image.open('Ethan.jpg')
 st.image(image5, caption='Ethan Woolley 2023')
 
 Charlotte_title = '<strong><p style="font-family:Optima; color:Black; font-size: 20px;">Charlotte Guillemot</p></strong>'
 st.markdown(Charlotte_title, unsafe_allow_html=True)
 rolecharlotte_title = '<p style="font-family:Optima; color:Black; font-size: 20px;">Role: Creative director </p>'
 st.markdown(rolecharlotte_title, unsafe_allow_html=True)
-image6 = Image.open('Charlotte.jpeg')
+image6 = Image.open('Charlotte.jpg')
 st.image(image6, caption='Charlotte Guillemot 2023')
 
 Georgina_title = '<strong><p style="font-family:Optima; color:Black; font-size: 20px;">Georgina Palmer</p></strong>'
 st.markdown(Georgina_title, unsafe_allow_html=True)
 rolegeorgina_title = '<p style="font-family:Optima; color:Black; font-size: 20px;">Role: CFO </p>'
 st.markdown(rolegeorgina_title, unsafe_allow_html=True)
-image7 = Image.open('Georgina.jpeg')
+image7 = Image.open('Georgina.jpg')
 st.image(image7, caption='Georgina Palmer 2023')
